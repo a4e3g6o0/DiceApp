470055e8ecc537d720b0182bf8a8879b244484b7
angel gomez <a4e3g6o0@hotmail.com>
2021-11-21 22:23:25 -0600

    Add roll dice function with random number

--- app/src/main/java/com/angel/diceapp/MainActivity.kt
+++ app/src/main/java/com/angel/diceapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.angel.diceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButtton = findViewById<Button>(R.id.btn_roll_dice);
+        rollButtton.setOnClickListener {
+            this.rollDice()
+        }
+    }
+
+    fun rollDice() {
+        val randomNumber = (1..6).random()
+        val randomNumberTxt = findViewById<TextView>(R.id.text_view_random_number)
+        randomNumberTxt.text = randomNumber.toString()
     }
 }
